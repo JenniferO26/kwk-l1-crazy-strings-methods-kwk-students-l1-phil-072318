a492507580fa17a23029ba430e82571d605f161d
Jennifer Okwara <jokwara03@gmail.com>
2018-07-24 15:12:42 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,11 @@
 # write your method here
 
 def crazy_strings(a,b)
-
+a = a.upcase.reverse
+b = b.swapcase.tr
+puts #{"crazy_strings"}
+# puts "crazy_strings".reverse
+# puts "crazy_strings".upcase
 end
+  crazy_strings
+  
